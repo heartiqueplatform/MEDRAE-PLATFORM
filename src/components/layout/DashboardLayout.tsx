61a47c3d49769b5e9f0e080497103cb5f7610bd6
HEARTIQUE PLATFORM <heartiqueofficial@gmail.com>
2025-09-13 15:18:02 +0300

    Update DashboardLayout.tsx

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -147,20 +147,23 @@ useEffect(() => {
 
   return (
     <SidebarProvider>
-      <div className="min-h-screen flex w-full bg-background">
-        <AppSidebar userRole={userRole} />
+     <div className="flex flex-col md:flex-row min-h-screen w-full bg-background">
+  <AppSidebar userRole={userRole} />
 
-        <div className="flex-1 flex flex-col">
-          <Header
-            user={user}
-            isDarkMode={isDarkMode}
-            onToggleDarkMode={toggleDarkMode}
-            streak={streak}
-          />
+  <div className="flex-1 flex flex-col min-h-screen">
+    <Header
+      user={user}
+      isDarkMode={isDarkMode}
+      onToggleDarkMode={toggleDarkMode}
+      streak={streak}
+    />
 
-          <main className="flex-1 p-4 md:p-6 overflow-auto">{children}</main>
-        </div>
-      </div>
+    <main className="flex-1 p-4 md:p-6 overflow-auto">
+      {children}
+    </main>
+  </div>
+</div>
+
     </SidebarProvider>
   );
 }
