2bae2f97617d64e8e84386c6e1ae221038b89a61
HEARTIQUE PLATFORM <heartiqueofficial@gmail.com>
2025-09-13 15:16:04 +0300

    Update student.tsx

--- src/pages/dashboard/student.tsx
+++ src/pages/dashboard/student.tsx
@@ -474,7 +474,7 @@ const fetchProgress = async () => {
   };
 
 return (
-  <div className="space-y-6">
+<div className="space-y-6 min-h-screen md:min-h-auto bg-gray-100 dark:bg-gray-900">
 
       {/* Welcome Section */}
 <div className="bg-gradient-to-r from-blue-800 via-blue-900 to-black rounded-xl p-6 text-white">
@@ -714,10 +714,7 @@ return (
       </div>
 <Card className="lg:col-span-3 w-full bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-700">
   <CardHeader>
-   <CardTitle className="text-gray-900 dark:text-white text-lg sm:text-xl break-words text-center">
-  Heartique Daily Status 💖
-</CardTitle>
-
+    <CardTitle className="text-gray-900 dark:text-white">Heartique Daily Status💖 </CardTitle>
     <CardDescription className="text-gray-700 dark:text-gray-300">
   This section is a space for nursing and medical professionals to share insights, reflections, and practical experiences. Contributions should be educational, thought-provoking, and meaningful, helping yourself and peers grow in knowledge and professional awareness. By sharing responsibly, you inspire others, spark discussions, and build a supportive learning community.
 </CardDescription>
