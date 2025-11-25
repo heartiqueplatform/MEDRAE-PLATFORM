f7053a3cacbe64f1bfb0bf19552517cca50f8394
HEARTIQUE PLATFORM <heartiqueofficial@gmail.com>
2025-11-25 15:26:57 +0300

    UPDATE

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.medrae.app',
+  appName: 'Medrae App',
+  webDir: 'dist'
+};
+
+export default config;
