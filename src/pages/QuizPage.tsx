6f20f57fb2e1ab4d302be28934e2ca40eae0e5c3
HEARTIQUE PLATFORM <heartiqueofficial@gmail.com>
2025-12-01 16:53:37 +0300

    Update QuizPage.tsx

--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -438,7 +438,7 @@ const handleResetTimer = () => {
       {/* Question Card */}
       <div
         className={`flex-1 p-4 border rounded shadow-sm border-gray-200 dark:border-gray-700 text-black dark:text-white transition-colors
-${understood[q.id] ? "bg-[#6FCF97] dark:bg-green-800" : ""}
+${understood[q.id] ? "bg-green-300 dark:bg-green-800" : ""}
 
         ${notUnderstood[q.id] ? "bg-[#FF4C4C] dark:bg-[#800000]" : "bg-white dark:bg-gray-800"}`}
       >
