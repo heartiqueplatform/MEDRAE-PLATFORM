63b6b8f06079783f7197902622c2f6aa6343d8d6
HEARTIQUE PLATFORM <heartiqueofficial@gmail.com>
2026-02-19 09:05:40 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -35,7 +35,7 @@ window.alert = function (message: AlertMessage) {
 */
 
 // App version for cache control
-const APP_VERSION = "63"// increment this with each deployment
+const APP_VERSION = "64"// increment this with each deployment
 const storedVersion = localStorage.getItem("appVersion");
 
 // Update app version safely without clearing all storage
