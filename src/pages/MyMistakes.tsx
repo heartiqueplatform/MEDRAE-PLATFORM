a8087cf131247357b729525058933caa39b21ee7
HEARTIQUE PLATFORM <heartiqueofficial@gmail.com>
2026-09-12 13:19:43 +0300

    Update MyMistakes.tsx

--- src/pages/MyMistakes.tsx
+++ src/pages/MyMistakes.tsx
@@ -488,7 +488,7 @@ export default function MyMistakes() {
                     <div className="absolute top-0 left-1/2 -translate-x-1/2 w-[120%] h-1.5 md:h-2 bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500 rounded-full blur-xl opacity-50" />
                     <div className="flex justify-between items-start">
                         <div>
-                            <h1 className="text-xl md:text-xl lg:text-4xl font-bold mb-2 md:mb-3 text-gray-900 dark:text-white relative z-10 flex items-center gap-2 md:gap-3">
+                            <h1 className="text-sm md:text-xl lg:text-4xl font-bold mb-2 md:mb-3 text-gray-900 dark:text-white relative z-10 flex items-center gap-2 md:gap-3">
 
                                 Hello there! You have <span className="font-bold text-red-600 dark:text-red-400">{mistakeCount}</span> unresolved {mistakeCount === 1 ? "mistake" : "mistakes"}.
 
